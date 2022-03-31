0658aa2373f7685c17082c397b719544ae7cc657
Baartooo <bartek.gajos19@gmail.com>
2022-03-31 21:05:03 +0200

    configure mantine in basic version

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import type { NextPage } from 'next';
+import { Button } from '@mantine/core';
 
 const Home: NextPage = () => {
-  return <>home</>;
+  return (
+    <div style={{ padding: 24 }}>
+      <Button>let&apos;s start!</Button>
+    </div>
+  );
 };
 
 export default Home;
