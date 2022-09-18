52784a23409063f130b9192228c4e2a5be24f7ef
Bartosz Gajos <bartosz.gajos@tsh.io>
2022-09-18 19:45:40 +0200

    improvement: change video sizes

--- src/ui/video/Video.styles.ts
+++ src/ui/video/Video.styles.ts
@@ -3,9 +3,9 @@ import { Styles } from '@/theme/theme.types';
 export const wrapperStyles: Styles = (theme) => ({
   marginTop: 48,
   width: '100%',
-  height: '55vw',
+  height: '52vw',
   [theme.fn.largerThan('md')]: {
-    height: 550,
+    height: 522,
   },
 });
 
